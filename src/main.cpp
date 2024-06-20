6879a9aa6f74f709d30bc74d3ab990240af89b62
SeaSparrow <contactMeOnNexus@gmail.com>
2024-06-20 13:39:41 +0300

    Update to allow for easier porting to older versions.

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,70 @@
-void SetupLog() {
-    auto logsFolder = SKSE::log::log_directory();
-    if (!logsFolder) SKSE::stl::report_and_fail("SKSE log_directory not provided, logs disabled.");
+#include <spdlog/sinks/basic_file_sink.h>
 
-    auto pluginName = Version::NAME;
-    auto logFilePath = *logsFolder / std::format("{}.log", pluginName);
-    auto fileLoggerPtr = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logFilePath.string(), true);
-    auto loggerPtr = std::make_shared<spdlog::logger>("log", std::move(fileLoggerPtr));
+namespace Log {
+    void SetupLog() {
+        auto logsFolder = SKSE::log::log_directory();
+        if (!logsFolder) SKSE::stl::report_and_fail("SKSE log_directory not provided, logs disabled.");
 
-    spdlog::set_default_logger(std::move(loggerPtr));
-    spdlog::set_level(spdlog::level::info);
-    spdlog::flush_on(spdlog::level::info);
+        auto pluginName = Version::PROJECT;
+        auto logFilePath = *logsFolder / std::format("{}.log", pluginName);
+        auto fileLoggerPtr = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logFilePath.string(), true);
+        auto loggerPtr = std::make_shared<spdlog::logger>("log", std::move(fileLoggerPtr));
 
-    //Pattern - For more information see here:
-    //https://github.com/gabime/spdlog/wiki/3.-Custom-formatting
-    spdlog::set_pattern("%v");
+        spdlog::set_default_logger(std::move(loggerPtr));
+        spdlog::set_level(spdlog::level::info);
+        spdlog::flush_on(spdlog::level::info);
+
+        //Pattern
+        spdlog::set_pattern("%v");
+    }
 }
-
+void MessageHandler(SKSE::MessagingInterface::Message* a_message) {
+    switch (a_message->type) {
+    case SKSE::MessagingInterface::kDataLoaded:
+        break;
+    case SKSE::MessagingInterface::kNewGame:
+    case SKSE::MessagingInterface::kPostLoadGame:
+        break;
+    default:
+        break;
+    }
+}
 
 #ifdef SKYRIM_AE
 extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
     SKSE::PluginVersionData v;
     v.PluginVersion({ Version::MAJOR, Version::MINOR, Version::PATCH });
-    v.PluginName(Version::NAME);
+    v.PluginName(Version::PROJECT);
     v.AuthorName(Version::AUTHOR);
     v.UsesAddressLibrary();
-    v.UsesNoStructs();
+    v.UsesUpdatedStructs();
     v.CompatibleVersions({
-        SKSE::RUNTIME_LATEST
-    });
+        SKSE::RUNTIME_1_6_1130,
+        _1_6_1170 });
     return v;
     }();
 #else 
-extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface * a_skse, SKSE::PluginInfo * a_info)
-{
-    a_info->infoVersion = SKSE::PluginInfo::kVersion;
-    a_info->name = Version::NAME.data();
-    a_info->version = 1;
-
-    const auto ver = a_skse->RuntimeVersion();
-    if (ver
-#	ifndef SKYRIMVR
-        < SKSE::RUNTIME_1_5_39
-#	else
-        > SKSE::RUNTIME_VR_1_4_15_1
-#	endif
-        ) {
-        SKSE::log::critical(FMT_STRING("Unsupported runtime version {}"), ver.string());
-        return false;
-    }
-    return true;
-}
+SKSEPluginInfo(
+    .Name = Version::PROJECT
+)
 #endif
 
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface * a_skse) {
-    SetupLog();
-    _loggerInfo("{} by {}", Version::NAME, Version::AUTHOR);
-    _loggerInfo("Plugin Version: {}.{}.{}", Version::MAJOR, Version::MINOR, Version::PATCH);;
+    Log::SetupLog();
+    _loggerInfo("Starting up {}.", Version::PROJECT);
+    _loggerInfo("Plugin Version: {}.{}.{}", Version::MAJOR, Version::MINOR, Version::PATCH);
+    _loggerInfo("Version build:");
 
-#ifndef SKYRIM_AE
-    _loggerInfo("1.5.97 Build - Maintained by {}.", Version::AUTHOR);
+#ifdef SKYRIM_AE
+    _loggerInfo("    >Latest Version. Maintained by: {}.", Version::AUTHOR);
+#else
+    _loggerInfo("    >1.5 Version. Do not report ANY issues with this version.");
 #endif
-
-    _loggerInfo("==================================================");
+    _loggerInfo("----------------------------------------------------------------");
 
     SKSE::Init(a_skse);
+    auto messaging = SKSE::GetMessagingInterface();
+    messaging->RegisterListener(MessageHandler);
+
     return true;
 }
