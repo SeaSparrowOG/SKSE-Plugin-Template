3dbc8be7233e9581ee66eb00332ba1dc96c11f28
SeaSparrow <SeaSparrow@NOT_REAL>
2026-02-10 14:44:02 +0200

    Define LoadFormStrings

--- src/Settings/JSON/JSONSettings.cpp
+++ src/Settings/JSON/JSONSettings.cpp
@@ -8,8 +8,22 @@ namespace Settings::JSON
 
 	Error LoadFormStrings(const Json::Value& a_value, std::vector<std::string>& a_result)
 	{
-		(void)a_value;
-		(void)a_result;
-		return Error::None;
+		if (a_value.isString()) {
+			a_result.push_back(a_value.asString());
+			return Error::None;
+		}
+		else if (a_value.isArray()) {
+			const auto size = a_value.size();
+			a_result.reserve(size);
+
+			for (const auto& value : a_value) {
+				if (!value.isString()) {
+					a_result.clear();
+					return Error::NonHomogenousArray;
+				}
+				a_result.push_back(value.asString());
+			}
+		}
+		return Error::NotStringOrArray;
 	}
 }
