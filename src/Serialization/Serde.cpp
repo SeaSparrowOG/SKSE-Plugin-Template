cea0f4de5a5151b5526ce2ca0158d6fe6832d23f
SeaSparrow <SeaSparrow@DESKTOP-QUF6TKB>
2026-02-12 00:40:36 +0200

    Serialization changes

--- src/Serialization/Serde.cpp
+++ src/Serialization/Serde.cpp
@@ -1,47 +1,213 @@
 #include "Serde.h"
 
-#include "SerializationManager/SerializationManager.h"
+namespace
+{
+	/// <summary>
+	/// Debug tool. When encountering unexpected RecordTypes, converts them to a readable string (HDEC, STEN, etc).
+	/// </summary>
+	/// <param name="a_typeCode">The unexpected record type.</param>
+	/// <returns>The unexpected record type as a string.</returns>
+	inline std::string DecodeTypeCode(std::uint32_t a_typeCode)
+	{
+		std::string result(4, '\0');
+
+		// Extract bytes from most significant to least
+		result[0] = static_cast<char>((a_typeCode >> 24) & 0xFF);
+		result[1] = static_cast<char>((a_typeCode >> 16) & 0xFF);
+		result[2] = static_cast<char>((a_typeCode >> 8) & 0xFF);
+		result[3] = static_cast<char>(a_typeCode & 0xFF);
+
+		return result;
+	}
+
+	/// <summary>
+	/// Helper function. Encodes a string into the interface.
+	/// </summary>
+	/// <param name="a_intfc">The serialization interface provided by SKSE.</param>
+	/// <param name="a_str">The string to serialize.</param>
+	/// <returns>True if encoding is successful, false otherwise.</returns>
+	inline static bool WriteString(SKSE::SerializationInterface* a_intfc,
+		const std::string& a_str)
+	{
+		std::size_t size = a_str.length() + 1;
+		return a_intfc->WriteRecordData(size) && a_intfc->WriteRecordData(a_str.data(), static_cast<std::uint32_t>(size));
+	}
+
+	/// <summary>
+	/// Helper function. Decodes a string from the interface, and stores it in a given variable.
+	/// </summary>
+	/// <param name="a_intfc">The serialization interface provided by SKSE.</param>
+	/// <param name="a_str">The result is stored here.</param>
+	/// <returns>True if successful, false otherwise.</returns>
+	inline static bool ReadString(SKSE::SerializationInterface* a_intfc,
+		std::string& a_str)
+	{
+		std::size_t size = 0;
+		if (!a_intfc->ReadRecordData(size)) {
+			return false;
+		}
+		a_str.reserve(size);
+		a_str.resize(size);
+		if (!a_intfc->ReadRecordData(a_str.data(), static_cast<std::uint32_t>(size))) {
+			return false;
+		}
+		if (!a_str.empty() && a_str.back() == '\0') {
+			a_str.pop_back();
+		}
+		return true;
+	}
+
+	/// <summary>
+	/// Helper function. Fetches the form found inside the serialization interface, and resolves it.
+	/// </summary>
+	/// <typeparam name="T">Cast the form as T</typeparam>
+	/// <param name="a_intfc">The serialization interface provided by SKSE.</param>
+	/// <returns>A pointer to T* if found, nullptr otherwise.</returns>
+	template <typename T>
+	inline static T* GetFormFromInterface(SKSE::SerializationInterface* a_intfc) {
+		RE::FormID oldID = 0;
+		if (!a_intfc->ReadRecordData(oldID)) {
+			return nullptr;
+		}
+		RE::FormID newID = 0;
+		if (!a_intfc->ResolveFormID(oldID, newID)) {
+			return nullptr;
+		}
+		return RE::TESForm::LookupByID<T>(newID);
+	}
+}
 
 namespace Serialization {
-	void SaveCallback(SKSE::SerializationInterface* a_intfc)
-	{
+	void SaveCallback(SKSE::SerializationInterface* a_intfc) {
 		logger::info("Starting save..."sv);
-		auto* serdeManager = SerializationManager::ObjectManager::GetSingleton();
+		auto* serdeManager = ObjectManager::GetSingleton();
 		if (!serdeManager) {
 			logger::critical("  >Failed to get internal serialization manager."sv);
-			return;
+			SKSE::stl::report_and_fail(fmt::format("{}:  Failed to save. Check the log for more information.", Plugin::NAME));
 		}
 		if (!serdeManager->Save(a_intfc)) {
-			logger::critical("  >Failed to save!"sv);
-			return;
+			SKSE::stl::report_and_fail(fmt::format("{}:  Failed to save. Check the log for more information.", Plugin::NAME));
 		}
 		logger::info("  >Save successful."sv);
 	}
 
-	void LoadCallback(SKSE::SerializationInterface* a_intfc)
-	{
+	void LoadCallback(SKSE::SerializationInterface* a_intfc) {
 		logger::info("Starting load..."sv);
-		auto* serdeManager = SerializationManager::ObjectManager::GetSingleton();
+		auto* serdeManager = ObjectManager::GetSingleton();
 		if (!serdeManager) {
 			logger::critical("  >Failed to get internal serialization manager."sv);
-			return;
+			SKSE::stl::report_and_fail(fmt::format("{}:  Failed to load. Check the log for more information.", Plugin::NAME));
 		}
 		if (!serdeManager->Load(a_intfc)) {
-			logger::critical("  >Failed to load!"sv);
-			return;
+			SKSE::stl::report_and_fail(fmt::format("{}:  Failed to load. Check the log for more information.", Plugin::NAME));
 		}
 		logger::info("  >Load successful."sv);
 	}
 
-	void RevertCallback(SKSE::SerializationInterface* a_intfc)
-	{
+	void RevertCallback(SKSE::SerializationInterface* a_intfc) {
 		logger::info("Starting revert..."sv);
-		auto* serdeManager = SerializationManager::ObjectManager::GetSingleton();
+		auto* serdeManager = ObjectManager::GetSingleton();
 		if (!serdeManager) {
 			logger::critical("  >Failed to get internal serialization manager."sv);
-			return;
+			SKSE::stl::report_and_fail(fmt::format("{}:  Failed to revert. Check the log for more information.", Plugin::NAME));
 		}
-		serdeManager->Save(a_intfc);
-		logger::info("  >Revert done."sv);
+		if (!serdeManager->Revert(a_intfc)) {
+			SKSE::stl::report_and_fail(fmt::format("{}:  Failed to revert. Check the log for more information.", Plugin::NAME));
+		}
+		logger::info("  >Revert successful."sv);
+	}
+
+	bool ObjectManager::Save(SKSE::SerializationInterface* a_intfc) {
+		bool success = true;
+		if (recordObjectMap.empty()) {
+			return success;
+		}
+
+		for (auto& obj : recordObjectMap) {
+			bool serializableSuccess = obj.second && obj.second->Save(a_intfc);
+			if (!serializableSuccess) {
+				logger::critical("  >Serialization error reported for object: {}"sv, obj.second ? 
+					DecodeTypeCode(obj.second->GetSerializationID()) : 
+					"NULL");
+			}
+			success &= serializableSuccess;
+		}
+		return success;
+	}
+
+	bool ObjectManager::Load(SKSE::SerializationInterface* a_intfc) {
+		bool success = true;
+		if (recordObjectMap.empty()) {
+			return success;
+		}
+
+		std::uint32_t type;
+		std::uint32_t version;
+		std::uint32_t length;
+		auto end = recordObjectMap.end();
+
+		while (a_intfc->GetNextRecordInfo(type, version, length)) {
+			auto it = recordObjectMap.find(type);
+			if (it != end) {
+				bool serializableSuccess = it->second && it->second->Load(a_intfc);
+				if (!serializableSuccess) {
+					logger::critical("  >Serialization error reported for object: {}"sv, DecodeTypeCode(type));
+				}
+				success &= serializableSuccess;
+			}
+		}
+		return success;
+	}
+
+	bool ObjectManager::Revert(SKSE::SerializationInterface* a_intfc) {
+		if (recordObjectMap.empty()) {
+			return true;
+		}
+
+		bool success = true;
+		for (auto& obj : recordObjectMap) {
+			bool serializableSuccess = obj.second && obj.second->Revert(a_intfc);
+			if (!serializableSuccess) {
+				logger::critical("  >Serialization error reported for object: {}"sv, obj.second ?
+					DecodeTypeCode(obj.second->GetSerializationID()) :
+					"NULL");
+			}
+			success &= serializableSuccess;
+		}
+		return success;
+	}
+
+	void ObjectManager::RegisterObject(Serializable& a_newObject) {
+		auto recordType = a_newObject.GetSerializationID();
+		if (recordObjectMap.contains(recordType)) {
+			SKSE::stl::report_and_fail(
+				fmt::format("Duplicate serialization registration for record type {}", DecodeTypeCode(recordType)));
+		}
+		recordObjectMap.emplace(recordType, &a_newObject);
+	}
+
+	void ObjectManager::UnRegisterObject(Serializable& a_object) {
+		auto it = recordObjectMap.find(a_object.GetSerializationID());
+		if (it != recordObjectMap.end()) {
+			recordObjectMap.erase(it);
+		}
+	}
+
+	bool Serializable::Register(std::uint32_t a_id) {
+		auto* manager = ObjectManager::GetSingleton();
+		if (!manager) {
+			logger::critical("Critical error registering serializable form: Internal ObjectManager returned invalid pointer."sv);
+			return false;
+		}
+
+		serdeID = a_id;
+		manager->RegisterObject(*this);
+		return true;
+	}
+
+	std::uint32_t Serializable::GetSerializationID() const { return serdeID; }
+
+	bool Serializable::Revert([[maybe_unused]] SKSE::SerializationInterface* a_intfc) {
+		return true;
 	}
 }
--- src/Serialization/Serde.h
+++ src/Serialization/Serde.h
@@ -4,8 +4,44 @@ namespace Serialization
 {
 	constexpr std::uint32_t Version = 1;
 	inline constexpr std::uint32_t ID = 'TRJT';
+	static_assert(Plugin::NAME == "TemplateProject"sv || ID != 'TRJT', "Make sure to use a unique ID for serialization.");
 
 	void SaveCallback(SKSE::SerializationInterface* a_intfc);
 	void LoadCallback(SKSE::SerializationInterface* a_intfc);
 	void RevertCallback(SKSE::SerializationInterface* a_intfc);
+
+	/// <summary>
+	/// Important notes:
+	/// * If Save, Load, or Revert fail, then the application state is considered corrupted and irrecoverable. Thus, the game closes.
+	/// * When calling Register, provide an a_id that is unique for EACH class. If a duplicate insertion is attempted, the game closes.
+	/// * Make sure to provide logging for each Save/Load/Revert failure as details. ObjectManager does note some things.
+	/// * A pointer to a Serializable class is stored in the ObjectManager singleton after it is registered. To move/delete/whatever, first Unregister it from the ObjectManager.
+	/// * Ideally, also inherit from REX::Singleton to deal with lifetime issues.
+	/// </summary>
+	class Serializable
+	{
+	public:
+		virtual bool Save(SKSE::SerializationInterface* a_intfc) = 0;
+		virtual bool Load(SKSE::SerializationInterface* a_intfc) = 0;
+		virtual bool Revert(SKSE::SerializationInterface* a_intfc);
+
+		bool Register(std::uint32_t a_id);
+		std::uint32_t GetSerializationID() const;
+	private:
+		std::uint32_t serdeID{ 0 };
+	};
+
+	class ObjectManager : public REX::Singleton<ObjectManager>
+	{
+	public:
+		bool Save(SKSE::SerializationInterface* a_intfc);
+		bool Load(SKSE::SerializationInterface* a_intfc);
+		bool Revert([[maybe_unused]] SKSE::SerializationInterface* a_intfc);
+
+		void RegisterObject(Serializable& a_newObject);
+		void UnRegisterObject(Serializable& a_object);
+
+	private:
+		std::unordered_map<uint32_t, Serializable*> recordObjectMap{};
+	};
 }
